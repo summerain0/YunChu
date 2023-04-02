e03d0b9998df79a3c232eea34c23707eeff6a383
summerain0 <2351602624@qq.com>
2023-04-02 22:52:43 +0800

    update proguard-rules.pro

--- yunchu-http/build.gradle.kts
+++ yunchu-http/build.gradle.kts
@@ -11,17 +11,6 @@ android {
     defaultConfig {
         minSdk = 21
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
 
     compileOptions {
