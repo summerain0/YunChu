4a9fb9d6f8a7c106342fa28e6b5ed9db4cd0d1b9
summerain0 <30911949+summerain0@users.noreply.github.com>
2023-11-29 23:07:39 +0800

    Update Versions.kt

--- build-logic/convention/src/main/kotlin/Versions.kt
+++ build-logic/convention/src/main/kotlin/Versions.kt
@@ -1,9 +1,9 @@
 @SuppressWarnings("unused")
 object Versions {
     const val versionName = "4.1.0"
-    const val compileSdkVersion = 33
+    const val compileSdkVersion = 34
     const val minSdkVersion = 21
-    const val targetSdkVersion = 33
+    const val targetSdkVersion = 34
     const val ndkVersion = "25.2.9519653"
     const val kotlinCompilerExtensionVersion = "1.4.7"
 }
